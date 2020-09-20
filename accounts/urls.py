77d1f66f1304c5ac775236aa5f64abf70a2a5a03
Alice Campkin <alice.campkin@gmail.com>
2020-09-20 11:40:15 +0100

    password reset functionality (no templates) added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
 from django.contrib.auth.views import LogoutView
 
@@ -6,4 +6,5 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('signup', views.SignUp.as_view(), name='signup'),
     path('profile/<int:pk>',views.UpdateUser.as_view(), name='profile'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
