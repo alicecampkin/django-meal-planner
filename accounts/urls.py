a1e4acafd358905ab81e76510d2d6e87a2b75047
Alice Campkin <alice.campkin@gmail.com>
2020-09-20 13:01:08 +0100

    custom templates added to password reset views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,51 @@
 from django.urls import include, path
 from . import views
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('signup', views.SignUp.as_view(), name='signup'),
     path('profile/<int:pk>',views.UpdateUser.as_view(), name='profile'),
-    path('accounts/', include('django.contrib.auth.urls')),
+    # path('accounts/', include('django.contrib.auth.urls')),
+
+    path('login',views.SignIn.as_view(), name='login'),
+
+    path('logout', views.LogoutView.as_view(), name='logout'),
+
+    path(
+        'accounts/password_change',
+        auth_views.PasswordChangeView.as_view(template_name='accounts/password_change_form.html'),
+        name='password_change'
+    ),
+
+    path(
+        'accounts/password_change/done',
+        auth_views.PasswordChangeDoneView.as_view(template_name='accounts/password_change_done.html'),
+        name='password_change_done'
+    ),
+    
+    path(
+        'accounts/password_reset',
+        auth_views.PasswordResetView.as_view(template_name='accounts/password_reset_form.html'),
+        name='password_reset'
+    ),
+
+    path(
+        'accounts/password_reset/done',
+        auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'),
+        name='password_reset_done'
+    ),
+
+    path(
+        'accounts/reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'),
+        name='password_reset_confirm'
+    ),
+
+    path(
+        'accounts/reset/done',
+        auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),
+        name='password_reset_complete',
+    )
+
 ]
