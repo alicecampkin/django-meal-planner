606eb27798a2d913e5d211e89ef6da1cbc7cc4b9
Alice Campkin <alice.campkin@gmail.com>
2020-10-09 22:18:35 +0100

    logs the user in upon successful registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
 from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -12,13 +13,15 @@ class SignUp(CreateView):
     model = User
     form_class = UserCreationForm
     template_name = 'accounts/signup.html'
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('create_profile')
 
     def form_valid(self, form):
-        """If the form is valid, save the associated model."""
-        self.object = form.save()
-
-        return super().form_valid(form)
+        view = super(SignUp, self).form_valid(form)
+        email, password = form.cleaned_data.get(
+            'email'), form.cleaned_data.get('password1')
+        user = authenticate(email=email, password=password)
+        login(self.request, user)
+        return view
 
 
 class SignIn(LoginView):
