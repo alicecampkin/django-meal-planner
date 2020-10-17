f1a819df7fec3b25fee0b6106cbc89c9f0b878cc
Alice Campkin <alice.campkin@zoodigital.com>
2020-10-17 13:24:55 +0100

    simplifies urls

--- core/urls.py
+++ core/urls.py
@@ -5,10 +5,10 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('lets-get-started', views.initialise_members, name='create_profile'),
     path('profile', views.update_primary_member, name='profile'),
-    path('household/members', views.list_members, name='list_members'),
-    path('household/members/add', views.AddMember.as_view(), name='add_member'),
-    path('household/members/<int:pk>/update',
+    path('members', views.list_members, name='list_members'),
+    path('members/add', views.AddMember.as_view(), name='add_member'),
+    path('members/<int:pk>/update',
          views.update_member, name='update_member'),
-    path('household/members/<int:pk>/delete',
+    path('members/<int:pk>/delete',
          views.DeleteMember.as_view(), name='delete_member')
 ]
