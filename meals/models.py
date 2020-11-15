15e852439d3c682ff3a13917898fdc44b3415899
Alice Campkin <alice.campkin@gmail.com>
2020-11-15 20:00:37 +0000

    ingredients model added

--- meals/models.py
+++ meals/models.py
@@ -3,12 +3,17 @@ from core.models import Member
 from accounts.models import User
 
 
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+
 class Meal(models.Model):
     name = models.CharField(max_length=255)
     date = models.DateField()
     members = models.ManyToManyField(Member)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     mealtime = models.CharField(max_length=255, null=True, blank=True)
+    ingredients = models.ManyToManyField(Ingredient)
 
     def __str__(self):
         return '%s' % self.name
